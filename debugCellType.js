513b6607a50a10f3c21efe40be732f9bd37e3a73
Ronald Kelley <kelro.privet@gmail.com>
2025-09-19 08:46:34 -0500

    Normalize to .gs; ignore .js; remove duplicates

--- debugCellType.js
+++
@@ -1,8 +0,0 @@
-function debugCellType() {
-  const sh = SpreadsheetApp.getActive().getSheetByName('People');
-  const val = sh.getRange(129, 3).getValue();  // Row 2, column 3 (DateOfBirth)
-  Logger.log('Value: %s', val);
-  Logger.log('Typeof: %s', typeof val);
-  Logger.log('Instanceof Date: %s', val instanceof Date);
-}
-
